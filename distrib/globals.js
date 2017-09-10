0f691fde9d71c6378ddf03cc5cfb1de7cd3fa395
Stephen Bohner <sbohner14335@users.noreply.github.com>
2017-09-10 13:11:21 -0400

    Changed App name and version
    Changed App name and version

--- distrib/globals.js
+++ distrib/globals.js
@@ -10,8 +10,8 @@
 //
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
-var APP_NAME = "TSOS"; // 'cause Bob and I were at a loss for a better name.
-var APP_VERSION = "0.07"; // What did you expect?
+var APP_NAME = "RollerballOS";   // The year is 2018 in a futuristic society where corporations have replaced countries.
+var APP_VERSION = "0.01"; // A violent futuristic game known as Rollerball is the recreational sport of the world.
 var CPU_CLOCK_INTERVAL = 100; // This is in ms (milliseconds) so 1000 = 1 second.
 var TIMER_IRQ = 0; // Pages 23 (timer), 9 (interrupts), and 561 (interrupt priority).
 // NOTE: The timer is different from hardware/host clock pulses. Don't confuse these.
