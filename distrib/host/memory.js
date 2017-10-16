59eb0d68ef24713b31a4d8d55d65cb2bb20bc3e0
Stephen Bohner <sbohner14335@users.noreply.github.com>
2017-10-16 15:47:28 -0400

    HTML Table memory column
    Created the first column of every row as the memory location

--- distrib/host/memory.js
+++ distrib/host/memory.js
@@ -6,12 +6,30 @@ var TSOS;
         }
         // Initializes the memory space (displayed in the process memory table).
         Memory.prototype.init = function () {
-            console.log(this.memory.length);
             var memoryTable = document.getElementById("memoryTable");
-            for (i = 0; i < this.memory.length - 8; i++) {
-                this.memory[i] = "00";
-                memoryTable.insertRow(i);
+            var j = 0; // Increments the row by 1 every 8 columns.
+            for (i = 0; i < this.memory.length; i+=8) {
+                var row = memoryTable.insertRow(j);
+                j++;
+                var cell0 = row.insertCell(0);
+                var cell1 = row.insertCell(1);
+                var cell2 = row.insertCell(2);
+                var cell3 = row.insertCell(3);
+                var cell4 = row.insertCell(4);
+                var cell5 = row.insertCell(5);
+                var cell6 = row.insertCell(6);
+                var cell7 = row.insertCell(7);
+                var cell8 = row.insertCell(8);
+                // Logic for parsing the cell data in the HTML table.
+                if (i < 10) {
+                    cell0.innerHTML = "0x" + "00" + i.toString() ;
+                } else if (i < 100){
+                    cell0.innerHTML = "0x" + i.toString();
+                } else {
+                    cell0.innerHTML = "0x" + i.toString();
+                }
             }
+
         };
         Memory.prototype.clearMemory = function () {
             for (i = 0; i < this.memory.length; i++) {
